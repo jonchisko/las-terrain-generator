aab99d99a9f13a953eed9bd79e40f57a3afb293e
Jon Škoberne <jonchisko@gmail.com>
2025-09-27 14:50:19 +0200

    feat: add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use exr::image::Encoding;
 use exr::image::SpecificChannels;
 use exr::math::Vec2;
@@ -10,9 +11,11 @@ use libblur::EdgeMode;
 use libblur::EdgeMode2D;
 use libblur::ThreadingPolicy;
 use std::error::Error;
+use std::fmt::Display;
 use std::fs;
 use std::num::NonZero;
 use std::path::PathBuf;
+use std::str::FromStr;
 use std::sync::Arc;
 use std::thread;
 
@@ -22,7 +25,240 @@ use kiddo::SquaredEuclidean;
 const DATA_DIR_PATH: &'static str = "./data/";
 const RESULTS_DIR_PATH: &'static str = "./results/";
 
+const MIN_POINT_DIM: u16 = 0;
+const MAX_POINT_DIM: u16 = 800;
+
+#[derive(Clone, Copy, Debug)]
+enum CommandlineParsingErrors {
+    NumberOfPointsAndRadius(&'static str),
+    IncorrectArgumentStructure(&'static str),
+    IntegerParsing(&'static str),
+}
+
+impl Display for CommandlineParsingErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result = match self {
+            CommandlineParsingErrors::NumberOfPointsAndRadius(msg) => {
+                format!("Number of points and radius, {}", msg)
+            }
+            CommandlineParsingErrors::IncorrectArgumentStructure(msg) => {
+                format!("Incorrect argument structure, {}", msg)
+            }
+            CommandlineParsingErrors::IntegerParsing(msg) => format!("Integer parsing, {}", msg),
+        };
+
+        f.pad(&result)
+    }
+}
+
+impl Error for CommandlineParsingErrors {}
+
+struct CorePointIterator {
+    start_position: (i16, i16),
+    current_position_index: (u8, u8),
+    side_dimension: u8,
+}
+
+impl Iterator for CorePointIterator {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_position_index.0 >= self.side_dimension
+            || self.current_position_index.1 >= self.side_dimension
+        {
+            return None;
+        }
+
+        let current_point = Point(
+            self.start_position.0 + self.current_position_index.0 as i16,
+            self.start_position.1 + self.current_position_index.1 as i16,
+        );
+
+        self.current_position_index.0 += 1;
+        if self.current_position_index.0 % self.side_dimension == 0 {
+            self.current_position_index.0 = 0;
+            self.current_position_index.1 += 1;
+        }
+
+        Some(current_point)
+    }
+}
+
+#[derive(Clone, Copy)]
+struct CorePoint {
+    center: Point,
+    radius: u8,
+}
+
+impl CorePoint {
+    fn new(center: Point, radius: u8) -> Self {
+        CorePoint { center, radius }
+    }
+
+    fn get_all_points_in_area(&self) -> CorePointIterator {
+        let side_dimension: u8 = (self.radius as u8)
+            .checked_mul(2)
+            .and_then(|prod| prod.checked_add(1))
+            .or(Some(255))
+            .unwrap();
+
+        let (start_x, start_y) = (
+            self.center.0 as i16 - self.radius as i16,
+            self.center.1 as i16 - self.radius as i16,
+        );
+
+        CorePointIterator {
+            start_position: (start_x, start_y),
+            current_position_index: (0u8, 0u8),
+            side_dimension,
+        }
+    }
+}
+
+impl TryFrom<&Cli> for Vec<CorePoint> {
+    type Error = CommandlineParsingErrors;
+
+    fn try_from(value: &Cli) -> Result<Self, Self::Error> {
+        let mut result = Vec::with_capacity(value.points.len());
+
+        for i in 0..value.points.len() {
+            let point = Point::from_str(&value.points[i])?;
+            result.push(CorePoint::new(point, value.radius[i]));
+        }
+
+        Ok(result)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct Point(i16, i16);
+
+impl FromStr for Point {
+    type Err = CommandlineParsingErrors;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut coordinates = s.split(",");
+
+        let first_number = coordinates
+            .next()
+            .and_then(|coord| Some(coord.trim_matches(|ch| ch == '(' || ch == ' ' || ch == ')')));
+        let second_number = coordinates
+            .next()
+            .and_then(|coord| Some(coord.trim_matches(|ch| ch == '(' || ch == ' ' || ch == ')')));
+
+        if None == first_number {
+            return Err(CommandlineParsingErrors::IncorrectArgumentStructure(
+                "First number is not correctly structured. Structure should be '(x, y)'",
+            ));
+        }
+
+        if None == second_number {
+            return Err(CommandlineParsingErrors::IncorrectArgumentStructure(
+                "Second number is not correctly structured. Structure should be '(x, y)'",
+            ));
+        }
+
+        let first_number = first_number.unwrap().parse::<i16>().map_err(|_err| {
+            CommandlineParsingErrors::IntegerParsing("First argument could not be converted to u16")
+        })?;
+        let second_number = second_number.unwrap().parse::<i16>().map_err(|_err| {
+            CommandlineParsingErrors::IntegerParsing("First argument could not be converted to u16")
+        })?;
+
+        Ok(Point(first_number, second_number))
+    }
+}
+
+struct Config {
+    possible_blocks: Vec<u8>,
+    blur_kernel_size: u8,
+    sample_size: u8,
+    resolution: u16,
+    destination_folder: String,
+}
+
+impl Config {
+    fn new(
+        possible_blocks: Vec<u8>,
+        blur_kernel_size: u8,
+        sample_size: u8,
+        resolution: u16,
+        destination_folder: String,
+    ) -> Self {
+        Config {
+            possible_blocks,
+            blur_kernel_size,
+            sample_size,
+            resolution,
+            destination_folder,
+        }
+    }
+}
+
+impl From<&Cli> for Config {
+    fn from(value: &Cli) -> Self {
+        Config::new(
+            value.possible_blocks.clone(),
+            value.blur_kernel_size,
+            value.sample_size,
+            value.resolution,
+            value.destination_folder.clone(),
+        )
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'p', required = true, value_delimiter = ' ', num_args = 1..)]
+    points: Vec<String>,
+
+    #[arg(short = 'r', required = true, value_delimiter = ' ', num_args = 1..)]
+    radius: Vec<u8>,
+
+    #[arg(long, required = true, value_delimiter = ' ', num_args = 1..)]
+    possible_blocks: Vec<u8>,
+
+    #[arg(short = 'b', default_value = "10")]
+    blur_kernel_size: u8,
+
+    #[arg(short = 's', default_value = "3")]
+    sample_size: u8,
+
+    #[arg(long, default_value = "1024")]
+    resolution: u16,
+
+    #[arg(short = 'd', required = true)]
+    destination_folder: String,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let arguments = Cli::parse();
+
+    if arguments.points.len() != arguments.radius.len() {
+        return Err(Box::new(CommandlineParsingErrors::NumberOfPointsAndRadius(
+            "Number of points must equal number of radius-es",
+        )));
+    }
+
+    let core_points = Vec::<CorePoint>::try_from(&arguments).unwrap();
+    let config = Config::from(&arguments);
+
+    /*
+    Now you have points and the config.
+    For each core_point get all points and insert it into a set structure (usually for small numbers a vec is faster),
+        if a point is valid - between 0,0 and 600,600
+    Now we have all the necessary points
+
+    Compute the number of threads that can be started and divide the load between them.
+    Each thread, goes over its points and requests the data - if data not there, continue (some points might not be valid url addresses)
+    Then you get the "bytes", basically the LAZ file and store it in your own vector.
+    At the end you join the threads and combine all the vectors into one data structure. These vectors reference the bytes or laz files.
+    These files are basically bytes hidden behind the cursor, so that reader can read them later on.
+
+    When you have got all this data, you basically do the rest of the already implemented program.
+     */
+
     let file_paths = get_files_in_directory()?;
     let (min_height, max_height) = get_height_bounds(&file_paths)?;
     let cpus = thread::available_parallelism()?;
